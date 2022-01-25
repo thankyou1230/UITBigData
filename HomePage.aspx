8d02b73c4a4babad56d36160941608a391b3115d
Quang Huỳnh Phan Minh <18520140@ms.uit.edu.vn>
2022-01-25 16:59:31 +0700

    first commit

---
+++ HomePage.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="HomePage.aspx.cs" Inherits="BIGDATAUIT.HomePage" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div style="margin-left: auto; margin-right: auto; text-align: center;">
+            <h1 style="text-align:center; font-weight:bold; margin-top:150px; margin-bottom:50; color:dodgerblue">LƯỢNG NGƯỜI ĐI QUA KHU VỰC SAU 1 TIẾNG NỮA</h1>
+            <asp:Label runat="server" ID="number" Text="345" Font-Size="100px" ForeColor="Orange"></asp:Label>
+            <p style="text-align:center; margin-top:100px; font-style:italic">Số liệu sẽ được cập nhật mỗi tiếng 1 lần</p>
+        </div>
+    </form>
+</body>
+</html>
---
+++ HomePage.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace BIGDATAUIT
+{
+    public partial class HomePage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string connetionString = null;
+            SqlConnection connection;
+            SqlCommand command;
+            string sql = null;
+            SqlDataReader dataReader;
+            connetionString = "Server=tcp:uittoptop.database.windows.net,1433;Initial Catalog=toptop;Persist Security Info=False;User ID=toptop;Password=0917787421qQ@;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+            sql = "Select * from BigDataNumber ORDER BY ID DESC";
+            connection = new SqlConnection(connetionString);
+            try
+            {
+                connection.Open();
+                command = new SqlCommand(sql, connection);
+                var numb = command.ExecuteScalar();
+                this.number.Text = numb.ToString();
+                command.Dispose();
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+}
